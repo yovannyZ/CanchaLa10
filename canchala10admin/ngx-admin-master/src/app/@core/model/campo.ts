ea4535887d38d824fb6853d3246c03dfbc7c033d
YovannyZ <zeballos.yovanny@gmail.com>
2018-02-15 23:02:09 -0500

    Admind

---
+++ canchala10admin/ngx-admin-master/src/app/@core/model/campo.ts
@@ -0,0 +1,14 @@
+export class Campo{
+    constructor(
+        public id:number,
+        public descripcion:string,
+        public implementos:string,
+        public estacionamiento:string,
+        public cantidad_jugadores:string,
+        public vestidores:string,
+        public snack:string,
+        public imagen:string,
+        public id_sede:number,
+        public estado:string
+    ){}
+}
---
+++ canchala10admin/ngx-admin-master/src/app/@core/model/sede.ts
@@ -0,0 +1,9 @@
+export class Sede{
+    constructor(
+        public id:number,
+        public descripcion:string,
+        public direccion:string,
+        public url_imagen:string,
+        public estado:boolean
+    ){}
+}
